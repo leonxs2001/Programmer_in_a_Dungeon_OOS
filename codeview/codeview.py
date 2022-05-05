36ac371fdb7cde0a97a08cf1e19617f4b14cf712
sxBUTCHxs <leonschoenberg@gmx.de>
2022-05-05 14:44:45 +0200

    code

--- codeview/codeview.py
+++ codeview/codeview.py
@@ -7,4 +7,5 @@ class CodeView(Level):
         self.image.fill((255,0,0))
         self.rect = self.image.get_rect()
     def draw(self, screen):
+        screen.fill((255,255,255))
         screen.blit(self.image,self.rect)
