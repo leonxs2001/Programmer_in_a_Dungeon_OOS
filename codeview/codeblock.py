a8855c125bec035d20407290f6598bf04fb5432a
sxBUTCHxs <leonschoenberg@gmx.de>
2022-05-06 11:32:19 +0200

    scrollment and movement of codeblocks

--- codeview/codeblock.py
+++ codeview/codeblock.py
@@ -1,30 +1,42 @@
 import pygame
 BACKGROUND_COLOR = (130,130,130)
 INVISIBLE_COLOR = (255,0,0)
-class codeBlock:
-    
+class CodeBlock(pygame.sprite.Sprite):
     def __init__(self):
+        super().__init__()
         self.size = pygame.Vector2(250,90)
         self.position = pygame.Vector2(10,10)
-        self.__build_image()
+        self.in_focus = False
+        self.position_in_image = pygame.Vector2(0,0)
+        self.__build_image(self.size)
 
-    def __build_image(self):
-        self.image = pygame.Surface(self.size)
+    def update_scale_factor(self, scalefactor):
+        last_scale_factor = self.rect.size[0] / self.size.x
+
+        self.__build_image(self.size * scalefactor)
+
+        center_of_scrollment = pygame.mouse.get_pos()
+        distance = (self.position - center_of_scrollment) / last_scale_factor
+        distance *= scalefactor
+        self.position = center_of_scrollment + distance
+    def __build_image(self, size):
+        self.image = pygame.Surface(size)
+        self.rect = self.image.get_rect()
         self.image.fill((255,255,255))
         self.image.set_colorkey(INVISIBLE_COLOR)#set the Color invisble
 
         border_size = 2
         #draw the normal rect with border
-        visible_size = self.size - pygame.Vector2(0,0.3*self.size.y)
+        visible_size = size - pygame.Vector2(0,0.3*size.y)
         rect = pygame.Rect((0,0),visible_size)
         pygame.draw.rect(self.image,BACKGROUND_COLOR,rect)
         pygame.draw.rect(self.image,(0,0,0),rect,width=border_size)
 
         #draw the bottom circle width border
-        factor = self.size.x/250
+        factor = size.x/250
         circle_overlap = 10 * factor
         circle_radius = 20 * factor
-        circle_x = self.size.x / 2
+        circle_x = size.x / 2
         pygame.draw.circle(self.image,BACKGROUND_COLOR,(circle_x ,visible_size.y - circle_overlap),circle_radius)
         pygame.draw.circle(self.image, (0,0,0), (circle_x ,visible_size.y - circle_overlap) ,circle_radius, width=border_size)
         #rect for covering the circlelines inside the rect
@@ -37,5 +49,18 @@ class codeBlock:
         pygame.draw.circle(self.image,INVISIBLE_COLOR,(circle_x ,-circle_overlap),circle_radius)
         pygame.draw.circle(self.image,(0,0,0),(circle_x ,-circle_overlap),circle_radius,width=border_size)
 
-    def draw(self, screen):
-        screen.blit(self.image, self.position)
+    def mouse_button_down(self):
+        mouse_position = pygame.mouse.get_pos()
+        #check collision with mouse
+        if self.rect.collidepoint(mouse_position):
+            self.in_focus = True
+            self.position_in_image = mouse_position - self.position
+
+    def mouse_button_up(self):
+        self.in_focus = False
+
+    def update(self):
+        if self.in_focus:
+            self.position = pygame.mouse.get_pos() - self.position_in_image
+
+        self.rect.topleft = self.position
--- codeview/codeview.py
+++ codeview/codeview.py
@@ -1,5 +1,7 @@
 import pygame
-from codeview.codeblock import codeBlock
+from pygame.locals import *
+from fight.bullet import Bullet
+from codeview.codeblock import CodeBlock
 from level import Level
 class CodeView(Level):
     def __init__(self):
@@ -7,11 +9,41 @@ class CodeView(Level):
         self.image = pygame.Surface((20,20))
         self.image.fill((255,0,0))
         self.rect = self.image.get_rect()
-        self.codeblock = codeBlock()
+        self.scale_factor = 1
+        self.last_mouse_position = pygame.Vector2(0,0)
+        self.is_mouse_button_down = False
+        self.code_block_group = pygame.sprite.Group()
+        self.code_block_group.add(CodeBlock())
+        
+        co = CodeBlock()
+        co.position = pygame.Vector2(100,100)
+        self.code_block_group.add(co)
     def give_event(self, event):
-        pass
+        if event.type == MOUSEBUTTONDOWN:
+            self.is_mouse_button_down = True
+            self.last_mouse_position = pygame.mouse.get_pos()
+            for code_block in self.code_block_group:
+                code_block.mouse_button_down()
+        elif event.type == MOUSEBUTTONUP:
+            self.is_mouse_button_down = False
+            for code_block in self.code_block_group:
+                code_block.mouse_button_up()
+        elif event.type == MOUSEWHEEL:
+            self.scale_factor += event.y/10
+            if self.scale_factor < 0.2:
+                self.scale_factor = 0.2
+            elif self.scale_factor > 4.7:
+                self.scale_factor = 4.7
+            for code_block in self.code_block_group:
+                code_block.update_scale_factor(self.scale_factor)
     def update(self):
-        pass
+        if self.is_mouse_button_down:
+            mouse_position = pygame.Vector2(pygame.mouse.get_pos())
+            distance = self.last_mouse_position - mouse_position
+            self.last_mouse_position = mouse_position
+            for code_block in self.code_block_group:
+                code_block.position -= distance
+        self.code_block_group.update()
     def draw(self, screen):
         screen.fill((255,255,255))
-        self.codeblock.draw(screen)
+        self.code_block_group.draw(screen)
