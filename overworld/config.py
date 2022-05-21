7eb26211cca6a161350cc054cff7bc31e50c05fe
Maged Tesare <maged.tesare@gmail.com>
2022-05-21 22:51:43 +0200

    added assets to config file

--- overworld/config.py
+++ overworld/config.py
@@ -1,9 +1,15 @@
 
 prepath = "overworld/assets"
 
-asset = {   "level": f"{prepath}/level.csv",
-            "wall": f"{prepath}/wall.png",
-            "outer_wall": f"{prepath}/outer_wall.png",
-            "ground": f"{prepath}/ground.png", 
-            "end": f"{prepath}/end.png",
-            "player": f"{prepath}/player.png"}
+asset = {
+       "level": f"{prepath}/level.csv",
+        "wall": f"{prepath}/wall.png",
+        "outer_wall": f"{prepath}/outer_wall.png",
+        "ground": f"{prepath}/ground.png", 
+        "end": f"{prepath}/end.png",
+        "player": f"{prepath}/player.png",
+        "melee_e": f"{prepath}/melee_e.png",
+        "big_melee_e": f"{prepath}/big_melee_e.png",
+        "shooting_e": f"{prepath}/shooting_e.png",
+        "big_shooting_e": f"{prepath}/big_shooting_e.png"
+        }
