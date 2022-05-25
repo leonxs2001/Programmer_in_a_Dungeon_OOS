43989af57773b1360b1170510f57e4009a9b7fef
Justixn <43824157+Justixn@users.noreply.github.com>
2022-05-25 05:10:14 +0200

    Welp, von 4 Klassen auf eine zurück

--- overworld/enemy.py
+++ overworld/enemy.py
@@ -3,36 +3,60 @@ from typing import List, Tuple
 import pygame
 
 class Melee_E(pygame.sprite.Sprite):
-        def __init__(self,assets,size,pos):
+        def __init__(self,assets,size,pos, move_points, image):
             pygame.sprite.Sprite.__init__(self)
-            self.image = pygame.transform.scale(pygame.image.load(assets['melee_e']),size)
+            self.image = image
             self.rect = self.image.get_rect()
             self.rect.topleft = pos
+            self.move_points = move_points
+            self.move_reset = move_points
+            self.assets = assets
+            self.uni_size = (40,40)
+        
+        def update(self, groups:List, assets, *args: any, **kwargs: any) -> None:
 
-class Big_Melee_E(pygame.sprite.Sprite):
-        def __init__(self,assets,size,pos):
-            pygame.sprite.Sprite.__init__(self)
-            self.image = pygame.transform.scale(pygame.image.load(assets['big_melee_e']),size)
-            self.rect = self.image.get_rect()
-            self.rect.topleft = pos
+            if self.move_points == 0:
+               
+                temp = pygame.sprite.Group()
 
+                rnd = randrange(4)
 
-class Shooting_E(pygame.sprite.Sprite):
-        def __init__(self,assets,size,pos):
-            pygame.sprite.Sprite.__init__(self)
-            self.image = pygame.transform.scale(pygame.image.load(assets['shooting_e']),size)
-            self.rect = self.image.get_rect()
-            self.rect.topleft = pos
+                if rnd == 0:                    
+                    temp.add(Melee_E(assets, self.uni_size, (self.rect.x-40, self.rect.y), 40, self.image))
 
-class Big_Shooting_E(pygame.sprite.Sprite):
-        def __init__(self,assets,size,pos):
-            pygame.sprite.Sprite.__init__(self)
-            self.image = pygame.transform.scale(pygame.image.load(assets['big_shooting_e']),size)
-            self.rect = self.image.get_rect()
-            self.rect.topleft = pos
+                        
+                if rnd == 1:              
+                    temp.add(Melee_E(assets, self.uni_size, (self.rect.x+40, self.rect.y), 40, self.image))
 
 
+                if rnd == 2:       
+                    temp.add(Melee_E(assets, self.uni_size, (self.rect.x, self.rect.y-40), 40, self.image))
+                    
+                if rnd == 3:                
+                    temp.add(Melee_E(assets, self.uni_size, (self.rect.x, self.rect.y+40), 40, self.image))
+                
+                if not(pygame.sprite.groupcollide(temp, groups, False,False)):
 
+                    if rnd == 0:
+                        self.rect.x -= 40
+                        
+                    if rnd == 1:
+                        self.rect.x += 40
+
+                    if rnd == 2:
+                        self.rect.y -= 40
+                            
+                    if rnd == 3:
+                        self.rect.y += 40
+
+                self.move_points = self.move_reset
+            else:
+                self.move_points -= 1
+                print(self.move_points)
+
+
+
+            return super().update(*args, **kwargs)
 
 
 class Mon(pygame.sprite.Sprite):
@@ -46,67 +70,23 @@ class Mon(pygame.sprite.Sprite):
             for x in range(len(self.done_map)):
                 for y in range(len(self.done_map[x])):
                     if (self.done_map[x][y])[0] == 'melee_e':
-                        self.monster = Melee_E(self.assets, self.uni_size, ((self.done_map[x][y])[1]))
+                        self.monster = Melee_E(self.assets, self.uni_size, ((self.done_map[x][y])[1]), 40, pygame.transform.scale(pygame.image.load(assets['melee_e']),self.uni_size))
                         self.monstergroup.add(self.monster)
                     if (self.done_map[x][y])[0] == 'big_melee_e':
-                        self.monster = Big_Melee_E(self.assets, self.uni_size, ((self.done_map[x][y])[1]))
+                        self.monster = Melee_E(self.assets, self.uni_size, ((self.done_map[x][y])[1]), 1, pygame.transform.scale(pygame.image.load(assets['big_melee_e']),self.uni_size))
                         self.monstergroup.add(self.monster)
                     if (self.done_map[x][y])[0] == 'shooting_e':
-                        self.monster = Shooting_E(self.assets, self.uni_size, ((self.done_map[x][y])[1]))
+                        self.monster = Melee_E(self.assets, self.uni_size, ((self.done_map[x][y])[1]), 40, pygame.transform.scale(pygame.image.load(assets['shooting_e']),self.uni_size))
                         self.monstergroup.add(self.monster)
                     if (self.done_map[x][y])[0] == 'big_shooting_e':
-                        self.monster = Big_Shooting_E(self.assets, self.uni_size, ((self.done_map[x][y])[1]))
+                        self.monster = Melee_E(self.assets, self.uni_size, ((self.done_map[x][y])[1]), 100, pygame.transform.scale(pygame.image.load(assets['big_shooting_e']),self.uni_size))
                         self.monstergroup.add(self.monster)
                    
 
-
-
     def update(self, groups:List):
 
-
-        for treasure in self.monstergroup:
-            
-            temp = pygame.sprite.Group()
-
-            rnd = randrange(4)
-
-            if rnd == 0:
-                    
-                temp.add(Melee_E(self.assets, self.uni_size, (treasure.rect.x-40, treasure.rect.y)))
-
-                    
-            if rnd == 1:
-                
-                temp.add(Melee_E(self.assets, self.uni_size, (treasure.rect.x+40, treasure.rect.y)))
-
-
-            if rnd == 2:
-        
-                temp.add(Melee_E(self.assets, self.uni_size, (treasure.rect.x, treasure.rect.y-40)))
-                
-            if rnd == 3:
-                
-                temp.add(Melee_E(self.assets, self.uni_size, (treasure.rect.x, treasure.rect.y+40)))
-            
-            if not(pygame.sprite.groupcollide(temp, groups, False,False)):
-
-                if rnd == 0:
-                    treasure.rect.x -= 40
-                    
-                if rnd == 1:
-                    treasure.rect.x += 40
-
-                if rnd == 2:
-                    treasure.rect.y -= 40
-                        
-                if rnd == 3:
-                    treasure.rect.y += 40
-
-
-
-
+        self.monstergroup.update(groups, self.assets)
       
-
 
     def draw(self, screen : pygame.Surface):
         self.monstergroup.draw(screen)
